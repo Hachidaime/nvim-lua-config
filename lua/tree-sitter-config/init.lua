9af6e4627d8cc741fc8970b79e561b4ad967ccf7
Hachidaime <cold.kid40@gmail.com>
2022-09-25 19:12:32 +0700

    Change parentheses rainbow colors

--- lua/tree-sitter-config/init.lua
+++ lua/tree-sitter-config/init.lua
@@ -17,8 +17,8 @@ require 'nvim-treesitter.configs'.setup {
   rainbow = {
     enable = true,
     extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    colors = { "#FFD700", "#DA70D6", "#179fff" }, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
   },
   autopairs = {
